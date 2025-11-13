509a060dee7a4cd4d0d7d07540db4abdb097b11b
timothyobosi <timothyobosi8@gmail.com>
2025-11-13 15:55:29 +0300

    handle users route

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common'
 
+//handle users route
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  /*
+    GET/users
+    GET/users/:id( pass param inside the url when we want to get id of 1 user)
+    POST/users 
+    PATCH /users/:id (id as param to identify the user)  
+    DELETE /users/:id 
+    */
+
+  @Get() //GET /users
+  findAll() {
+    return []
+  }
+
+  // @Get('interns') // GET /users/interns
+  // findAllInterns(){
+  //     return []
+  // }
+
+  @Get(':id') //GET /users/:id
+  findOne(@Param('id') id: string) {
+    return { id }
+  }
+
+  @Post() //Post /users
+  create(@Body() user:{}){
+    return user
+  }
+
+  @Patch(':id')//PATCH /users/:id
+  update(@Param('id') id:string, @Body() userUpdate:{}){
+    return {id, ...userUpdate}
+  }
+
+  @Delete(':id') //DELETE /users/:id
+  delete(@Param('id') id: string){
+    return {id}
+  }
+
+}
