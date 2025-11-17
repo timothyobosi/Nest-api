f36970c560ce90d7ab9f0e3c79bbe062c2fd05e1
timothyobosi <timothyobosi8@gmail.com>
2025-11-17 10:30:41 +0300

    find all method

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@
 import eslint from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
+import { single } from 'rxjs';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
@@ -32,6 +33,8 @@ export default tseslint.config(
       'prettier/prettier': [
         'error',
         {
+          singleQuote: true,
+          quoteProps: 'as-needed',
           semi: false,
           trailingComma: 'none',
           endOfLine: 'auto'
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,54 @@
 import { Injectable } from '@nestjs/common'
 
-@Injectable()
-export class UsersService {}
+@Injectable() //Decorator- attach metadata that deaclreas this class we are creating
+export class UsersService {
+    private users = [
+        //user objects -> Properety inside userService class
+        {
+            "id" : 1,
+            "name" : "Leann Graham",
+            "email" : "sincere@april.biz",
+            "role" : "INTERN",
+        },
+        {
+            "id" : 2,
+            "name" : "Ervin Howell",
+            "email" : "Shanna@melisasa.tv",
+            "role" : "INTERN",
+        },
+        {
+            "id" : 3,
+            "name" : "Clementine Bauch",
+            "email" : "clement@bauch.tv",
+            "role" : "ENGINEER",
+        },
+        {
+            "id" : 4,
+            "name" : "Patricia Lebsack",
+            "email" : "par@leback.tv",
+            "role" : "ENGINEER",
+        },
+        {
+            "id" : 5,
+            "name" : "Chelsey Dietrich",
+            "email" : "lauciohettinger@annie.ca",
+            "role" : "ADMIN",
+        }     
+
+    ]
+
+
+    findAll(role?: 'INTERN'|'ENGINEER'|'ADMIN'){
+        //create what goes inside the method
+         if(role){ // since its is an arrya we can filter to return users with the role specified
+            return this.users.filter(user => user.role === role)//user in our filter which is a high order array method
+         }// and we want it to match the role passed in as argument
+         return this.users //if no role wsa passed return all users
+
+    }
+
+    findOne(id: number){
+        const user 
+    }
+
+}
