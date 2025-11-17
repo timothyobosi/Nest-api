7cedf733ad5436a8480a2aaf73363a3c07d5cf1d
timothyobosi <timothyobosi8@gmail.com>
2025-11-17 11:01:42 +0300

    complete users service class

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -47,8 +47,59 @@ export class UsersService {
 
     }
 
-    findOne(id: number){
-        const user 
+    findOne(id: number){ //finda on method, come in with an id, 
+    // its going to be a number when this method recieves it
+        const user = this.users.find(user => user.id === id)// users is another array method//user id needs to match the id number
+
+        return user
     }
+
+    //Receive a user, will be an object, we specify what is going to be in object
+    //in past we had empty oject when we created our controller route
+    create(user: {name: string, email: string: string, role: 'INTERN'|
+        'ENGINEER'|'ADMIN'}){
+            //ID will be auto generated
+            //logic to create that id    //create new array
+            const userByHighestId = [...this.users].sort((a,b) => b.id -a.id)
+            const newUser ={
+                //define a new user object
+                //pick an id and generate a new one 
+                //have an id that will I add to this user that we recieve as name email and role
+                id : userByHighestId[0].id + 1, //get the first element in the sorted array and get its id and add 1 to it
+                ...user //spread operator to get the rest of the properties from the user object passed in
+                //all this to generate that id that is not already in the data
+            }
+            this.users.push(newUser)//push the new user to the users array
+            return newUser//return the new user created
+        }
+
+        update(id: number, updatedUser:{name:string, email:string, // we cant update id but we can update other 3 properties
+             role?:'INTERN' | 'ENGINEER' | 'ADMIN'})//? means optional for the update
+             {  
+                this.users = this.users.map(user => {// this will spread all the properties of the user 
+                    if(user.id === id){
+                        return{...user, ...updatedUser}//the updated user will overwrite the existing user properties
+                    }
+                    return user //if the id does not match return the user as is
+                })
+
+                
+                return this.findOne(id) //return the updated user by calling findOne method with the id
+             }
+
+             //create the delete method, will recieve the id only
+             delete(id: number){
+                //inside the method we define the removedUser and defined seperately
+                const removedUSer = this.findOne(id) //find the user to be removed
+
+                this.users = this.users.filter(user => user.id !!== id) //Excludes that user that is needed to be removed
+                //filter out the user with the matching id
+
+                return removedUSer //return the removed user
+
+             }     
+
+
+
 
 }
